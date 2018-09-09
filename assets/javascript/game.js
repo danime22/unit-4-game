8e910bf42b9de6040456499fd91d8c49f8c2427f
Yan Davdison <yandavdison@Yans-MacBook-Pro.local>
2018-09-09 00:48:33 -0500

    unit

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    var characters = {
+
+    }
+
+    var character1= "";
+    var character2= "";
+    var weapon="";
+
+
+
+
+
+$(".butt").on("click", function( ) {
+    alert("hey");
+})
+
+
+
+}); 
+
