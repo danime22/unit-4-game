397ac2696ab61a626c0c15379fae6653f7d31d7a
Yan Davdison <yandavdison@Yans-MacBook-Pro.local>
2018-09-10 17:46:06 -0500

    unit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,105 @@
 
-// var matrix = {
-//         character: {
-//             name: ["neo", "trinity", "morpheus", "agent smith"]
-//         },
-//         weapon: {
-//             weapy: ["", "", "", ""]
-//         }
+var matrix = [
+    {
+        name: "NEO",
+        attack: "",
+        life: 100,
+        song:"assets/The Matrix  Soundtrack- Juno Reactor Vs Don Davis - Navras.mp3",
+        line:""
+    },
+    {
+        name: "TRINITY",
+        attack: "",
+        life: 100,
+        song:"assets/The Matrix  Soundtrack- Juno Reactor Vs Don Davis - Navras.mp3",
+        line:""
+    },
 
-// }
+    {
+        name: "AGENT SMITH",
+        attack: "",
+        life: 100,
+        song:"assets/The Matrix  Soundtrack- Juno Reactor Vs Don Davis - Navras.mp3",
+        line:""
+    },
+    {
+        name: "MORPHEUS",
+        attack: "",
+        life: 100,
+        song:"assets/The Matrix  Soundtrack- Juno Reactor Vs Don Davis - Navras.mp3",
+        line:""
+    }
 
 
+];
+
+
+var weapon = [
+    {
+        weapon1: "gun1",
+        damage: 13,
+       
+    },
+    {
+        weapon1: "gun2",
+        damage: 7,
+       
+
+    },
+    {
+        weapon1: "gun3",
+        damage: 8,
+        
+
+    },
+    {
+        weapon1: "sword",
+        damage: 5,
+       
+    }
+];
 
 // var attack="";
+// var gameState= {
+
+
+// }
 var character1 = "";
 var character2 = "";
 var weapons = "";
 var damage = "";
 var life= "";
-var userChosen=false;
-matrixState="";
+var userChosen = false;
+matrixState = "";
 
 
+$("myAudio").src=matrix.song;
 
 $(document).ready(function () {
-  
+
+ 
 
     $("#neo1").on("click", function () {
-     if(userChosen) {
-         $("#neo1").appendTo("#charEnemy");
-     } else {
-         $("#neo1").appendTo("#charSelect");
-         userChosen = true;
-     }
        
+        if (userChosen) {
+            $("#neo1").appendTo("#charEnemy");
+            
+        } else {
+            $("#neo1").appendTo("#charSelect");
+            userChosen = true;
+        }
+
         $("#span1").remove();
-  
+        setText();
+        
+       
     });
 
     $("#trinity1").on("click", function () {
         if (userChosen) {
             $("#trinity1").appendTo("#charEnemy");
-            
+           
+
         } else {
             $("#trinity1").appendTo("#charSelect");
             userChosen = true;
@@ -53,6 +112,7 @@ $(document).ready(function () {
     $("#agent1").on("click", function () {
         if (userChosen) {
             $("#agent1").appendTo("#charEnemy");
+          
         } else {
             $("#agent1").appendTo("#charSelect");
             userChosen = true;
@@ -65,6 +125,7 @@ $(document).ready(function () {
     $("#morpheus1").on("click", function () {
         if (userChosen) {
             $("#morpheus1").appendTo("#charEnemy");
+            
         } else {
             $("#morpheus1").appendTo("#charSelect");
             userChosen = true;
@@ -73,9 +134,17 @@ $(document).ready(function () {
         $("#span4").remove();
 
     });
-        
 
-
+   
 
 });
+
+function setText() {
+    var set= $(".card-title").text("Choose Your Enemy");
+    var weapy = $("#weapy").text("Choose your weapon");
+    
+}
+
+
+
 
