aad2d9506141c97ece8b9741cb9f83100e660c48
Yan Davidson <yan@Dave-Davidsons-iMac.local>
2018-09-09 21:15:20 -0500

    unit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,15 @@
 
-var matrix = {
-        character: {
-            name: ["neo", "trinity", "morpheus", "agent smith"]
-        },
-        weapon: {
-            weapy: ["", "", "", ""]
-        }
+// var matrix = {
+//         character: {
+//             name: ["neo", "trinity", "morpheus", "agent smith"]
+//         },
+//         weapon: {
+//             weapy: ["", "", "", ""]
+//         }
 
-}
+// }
+
+
 
 // var attack="";
 var character1 = "";
@@ -15,19 +17,63 @@ var character2 = "";
 var weapons = "";
 var damage = "";
 var life= "";
+var userChosen=false;
+matrixState="";
 
 
 
 $(document).ready(function () {
-    var characters = {
+  
 
-    }
+    $("#neo1").on("click", function () {
+     if(userChosen) {
+         $("#neo1").appendTo("#charEnemy");
+     } else {
+         $("#neo1").appendTo("#charSelect");
+         userChosen = true;
+     }
+       
+        $("#span1").remove();
+  
+    });
+
+    $("#trinity1").on("click", function () {
+        if (userChosen) {
+            $("#trinity1").appendTo("#charEnemy");
+            
+        } else {
+            $("#trinity1").appendTo("#charSelect");
+            userChosen = true;
+        }
+        $("#span2").remove();
 
 
-    $(".butt").on("click", function () {
-        // alert("hey");
-        $()
-    })
+    });
+
+    $("#agent1").on("click", function () {
+        if (userChosen) {
+            $("#agent1").appendTo("#charEnemy");
+        } else {
+            $("#agent1").appendTo("#charSelect");
+            userChosen = true;
+        }
+        $("#span3").remove();
+
+
+    });
+
+    $("#morpheus1").on("click", function () {
+        if (userChosen) {
+            $("#morpheus1").appendTo("#charEnemy");
+        } else {
+            $("#morpheus1").appendTo("#charSelect");
+            userChosen = true;
+        }
+
+        $("#span4").remove();
+
+    });
+        
 
 
 
